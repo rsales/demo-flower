c2ff15f1465739d6f9e6b2d29a0680f1991bdda6
rsales <rafael.sales93@hotmail.com>
2022-04-22 15:41:28 -0300

    Update Favicon.

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '🌷 Demo Flowers',
+    title: 'Demo Flowers',
     htmlAttrs: {
       lang: 'en',
     },
